e726d6d0f6cbe150d71d5d8fecc0ae55fe1aeb6d
teksander <alexandre.melo.pacheco@gmail.com>
2024-11-27 19:10:35 -0100

    refactored MessageHandler

--- src/connections/MessageHandler.py
+++ src/connections/MessageHandler.py
@@ -6,99 +6,135 @@ import logging
 logger = logging.getLogger('w3')
 
 class MessageHandler:
+
     def __init__(self, node_server):
+
         self.node_server = node_server
         self.node = node_server.node
         self.enode = self.node.enode
+
+        # Message type to handler  mappings 
+        self.requests_handler_mapping = {
+            MEMPOOL_SYNC_TAG: self.handle_request_mempool,
+            CHAIN_SYNC_TAG: self.handle_request_sync,
+            BLOCK_REQUEST_TAG: self.handle_request_block,
+            }
+
+        self.answers_handler_mapping = { 
+            CHAIN_SYNC_TAG: self.handle_answer_sync,
+            MEMPOOL_SYNC_TAG: self.handle_answer_mempool,
+            BLOCK_REQUEST_TAG: self.handle_answer_block,
+            }
 
     def handle_request(self, msg):
         """
         Returns a message containing the requested information
         """
-        if not self.check_message_validity(msg):
-            logger.error("invalid message")
+        if not self.validate_message(msg):
             return
 
-        if DEBUG:
-            logger.debug(msg)
-
-        msg_type = msg["type"]
-
-        if msg_type == MEMPOOL_SYNC_TAG:
-            transaction_list = list(self.node.mempool.values())
-            content = []
-            for t in transaction_list:
-                content.append(transaction_to_dict(t))
-            return self.construct_message(content, MEMPOOL_SYNC_TAG)
-
-        elif msg_type == CHAIN_SYNC_TAG:
-            last_block = self.node.get_block('last')
-            content = (last_block.get_header_hash(), last_block.total_difficulty)
-            return self.construct_message(content, CHAIN_SYNC_TAG)
-
-        elif msg_type == BLOCK_REQUEST_TAG:
-            content = self.handle_block_request(msg["data"])
-            return self.construct_message(content, BLOCK_REQUEST_TAG)
+        handler = self.requests_handler_mapping.get(msg["type"])
+        content = handler(msg)
+        return self.construct_message(content, msg["type"])
 
     def handle_answer(self, msg):
-        if not self.check_message_validity(msg):
-            logger.error("invalid message")
+        """
+        Processes the answer based on its type.
+        """
+        if not self.validate_message(msg):
             return
 
-        if DEBUG:
-            logger.debug(f"Node {self.node.id} received {msg}")
-
-        msg_type = msg["type"]
-        if msg_type == CHAIN_SYNC_TAG:
-            self.handle_chain_sync_answer(msg)
-
-        elif msg_type == MEMPOOL_SYNC_TAG:
-            self.update_mempool(msg["data"])
-
-        elif msg_type == BLOCK_REQUEST_TAG:
-            self.handle_block_answer(msg)
-
-    def check_message_validity(self, message):
-        mandatory_keys = ["data", "type", "receiver", "sender"]
-        if isinstance(message, dict):
-            for key in mandatory_keys:
-                if key not in message:
-                    logger.error(f"Invalid key {message}")
-                    return False
-            return True
-        logger.error(f"Invalid message {message}")
-        return False
+        handler = self.answers_handler_mapping.get(msg["type"])
+        handler(msg)
 
     def construct_message(self, data, msg_type, receiver=None):
-        message = {"type": msg_type, "receiver": receiver, "sender": self.enode, "data": data}
-        return message
+        return {
+                "type": msg_type,
+                "receiver": receiver,
+                "sender": self.enode,
+                "data": data
+                }
 
-    def update_mempool(self, transactions):
-        _list = []
-        for d in transactions:
-            _list.append(dict_to_transaction(d))
-        self.node.sync_mempool(_list)
+    def validate_message(self, msg):
+        mandatory_keys = {"data", "type", "receiver", "sender"}
+        if not isinstance(msg, dict):
+            logger.error(f"Invalid message format: {msg}")
+            return False
 
-    def handle_chain_sync_answer(self, message):
-        last_block = self.node.get_block('last')
-        if message["data"] == (last_block.get_header_hash(), last_block.total_difficulty):
-            # Chains are synchronised
-            if constants.DEBUG:
-                logger.debug(f"Node {self.node.id} is chain sync")
+        missing_keys = mandatory_keys - msg.keys()
+        if missing_keys:
+            logger.error(f"Message missing keys {missing_keys}: {msg}")
+            return False
+
+        return True
+
+    ################# REQUEST HANDLERS ########################
+    def handle_request_sync(self, msg):
+        """ Returns the latest hash and difficulty """
+        return (self.node.get_block('last').get_header_hash(), self.node.get_block('last').total_difficulty)
+
+    def handle_request_mempool(self, msg):
+        """ Returns the current mempool as a list """
+        return [transaction_to_dict(t) for t in self.node.mempool.values()]
+
+    def handle_request_block(self, msg):
+        """ Checks if one of the indicated blocks is in its chain """
+           
+        for header_hash, height in msg["data"]:
+
+            potential_common_block = self.node.get_block(height)
+            if potential_common_block is None:
+                return None, None
+
+            if header_hash == potential_common_block.get_header_hash():
+                # Common block found
+                partial_chain = []
+                i = height + 1
+                while i < self.node.current_height:
+                    partial_chain.append(block_to_list(self.node.get_block(i)))
+                    i += 1
+                return height, partial_chain
+        return height, None
+
+    ################# ANSWER HANDLERS  ########################
+
+    def handle_answer_sync(self, msg):
+
+        peer_hash, peer_difficulty   = msg["data"]
+        local_hash, local_difficulty = self.node.get_sync_info()
+
+        # Case 1: My chain is already synchronized with the peer
+        if local_hash == peer_hash:
             return
 
-        if last_block.total_difficulty <= message["data"][1]:
-            # If the chains have equal sizes, node keeps his
-            # If the chain of the node is longer than the received one, let him do the work
-            self.request_block(len(self.node.chain), message["sender"])
+        # Case 2: My chain is longer or has equal difficulty
+        elif local_difficulty >= peer_difficulty:
+            return
+
+        # Case 3: Peer has longer chain
         else:
-            if constants.DEBUG:
-                logger.debug(f"Node {self.node.id} has a current diff of {last_block.total_difficulty}")
+            self.request_block(self.node.current_height, msg["sender"])
+
+    def handle_answer_mempool(self, msg):
+        
+        self.node.sync_mempool([dict_to_transaction(d) for d in msg["data"]])
+
+    def handle_answer_block(self, msg):
+
+        height, partial_chain = msg["data"]
+
+        if height is None:
+            return
+
+        if partial_chain is None:
+            self.request_block(height, msg["sender"])
+            
+        elif len(partial_chain) > 0:
+            self.node.sync_chain(partial_chain, height)
 
     def request_block(self, current_height, enode):
-        """
-        Sends the last 5 blocks header hash
-        """
+        """ Send the last 5 blocks header hash """
+    
         content = []
         # Sends the block header + height of the last 5 blocks before the precised height
         for block in reversed(self.node.chain[max(0, current_height - 5):current_height]):
@@ -107,35 +143,25 @@ class MessageHandler:
         request = self.construct_message(content, BLOCK_REQUEST_TAG, enode)
         self.node_server.send_request(enode, request)
 
-    def handle_block_request(self, block_info_list):
-        """
-        Checks if one of the indicated blocks is in its chain
-        """
-        for block_info in block_info_list:
-            block_header_hash = block_info[0]
-            height = block_info[1]
-            potential_common_block = self.node.get_block(height)
-            if potential_common_block is None:
-                return None, None
-            if block_header_hash == potential_common_block.get_header_hash():
-                # Common block found
-                partial_chain = []
-                i = height + 1
-                while i < len(self.node.chain):
-                    partial_chain.append(block_to_list(self.node.get_block(i)))
-                    i += 1
-                return height, partial_chain
-        return height, None
 
-    def handle_block_answer(self, msg):
-        data = msg["data"]
-        height = data[0]
-        chain = data[1]
 
-        if height is None:
-            return
 
-        if chain is None:
-            self.request_block(height, msg["sender"])
-        elif len(chain) > 0:
-            self.node.sync_chain(chain, height)
+
+
+# OLD VERSIONS
+
+    # def handle_answer_sync(self, msg):
+    #     last_block = self.node.get_block('last')
+    #     if msg["data"] == (last_block.get_header_hash(), last_block.total_difficulty):
+    #         # Chains are synchronised
+    #         if constants.DEBUG:
+    #             logger.debug(f"Node {self.node.id} is chain sync")
+    #         return
+
+    #     if last_block.total_difficulty <= msg["data"][1]:
+    #         # If the chains have equal sizes, node keeps his
+    #         # If the chain of the node is longer than the received one, let him do the work
+    #         self.request_block(len(self.node.chain), msg["sender"])
+    #     else:
+    #         if constants.DEBUG:
+    #             logger.debug(f"Node {self.node.id} has a current diff of {last_block.total_difficulty}")
